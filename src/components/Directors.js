8a5ed364b810e449fa766b67283ee6f79f01905d
Lehane-P <Lehane-P>
2023-09-06 22:43:22 +0300

    done

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const directorList= directors.map((dir)=>{
+   
+    const moviesList = (dir.movies).map((mov)=><li key={Math.floor(Math.random()  * (1000 - 100)) + 100}>{mov}</li>)
+      return (
+        <div>
+           <h1>{dir.name}</h1>
+           <ul>
+             {moviesList}
+           </ul>
+        </div>)                                 
+  })
+
+
+  return(
+   <div>
+    {/*{code here}*/}
+    <h1>Directors Page</h1>
+        {directorList}
+    </div>
+  )
 }
 
 export default Directors;
